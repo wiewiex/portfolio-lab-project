f70ed478b364c92ed1a4ab633e9d3d77acdf33c2
Jacek Pietrzak <wiewiex@gmail.com>
2022-05-23 09:49:56 +0200

    sekcja proste kroki gotowa

--- components/home/SimpleSteps.js
+++ components/home/SimpleSteps.js
@@ -3,9 +3,39 @@ import style from "../../styles/home/SimpleSteps.module.scss"
 export default function SimpleSteps() {
     return(
         <section className={style.simpleStepsContainer}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
+            <div className={style.title}>
+                <h2>Wystarczą 4 proste kroki</h2>
+                <img src="/assets/Decoration.svg"/>
+            </div>
+            <div className={style.steps}>
+                <div className={style.singleCard}>
+                    <img src="/assets/Icon-1.svg"/>
+                    <h3>Wybierz rzeczy</h3>
+                    <div/>
+                    <p>ubrania, zabawki,<br/>sprzęt i inne</p>
+                </div>
+                <div className={style.singleCard}>
+                    <img src="/assets/Icon-2.svg"/>
+                    <h3>Spakuj je</h3>
+                    <div/>
+                    <p>ubrania, zabawki,<br/>sprzęt i inne</p>
+                </div>
+                <div className={style.singleCard}>
+                    <img src="/assets/Icon-3.svg"/>
+                    <h3>Zdecyduj komu chcesz pomóc</h3>
+                    <div/>
+                    <p>ubrania, zabawki,<br/>sprzęt i inne</p>
+                </div>
+                <div className={style.singleCard}>
+                    <img src="/assets/Icon-4.svg"/>
+                    <h3>Zamów kuriera</h3>
+                    <div/>
+                    <p>ubrania, zabawki,<br/>sprzęt i inne</p>
+                </div>
+            </div>
+            <div className={style.cta}>
+                <h2>ODDAJ<br/>RZECZY</h2>
+            </div>
         </section>
     )
 }
